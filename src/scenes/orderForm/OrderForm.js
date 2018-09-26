78e18f7c9e3a75241f5e50e7dec92bbd5560f4b4
Carlos de la Sierra <carlos@yeahbetty.com>
2018-09-26 14:08:17 +1000

    saved to check master codebase - no working commit

--- src/scenes/orderForm/OrderForm.js
+++ src/scenes/orderForm/OrderForm.js
@@ -3,19 +3,9 @@ import React, { Component } from 'react';
 import ShoppingCart from '../../components/shoppingCart/ShoppingCart';
 import WizardContainer from './wizard/WizardContainer';
 import Timeline from './timeline/Timeline';
-import StepModel from './wizard/steps/StepModel';
 
 import { STEP_ROUTES } from './OrderFormRoutes';
 import { MENU_LABELS } from './OrderFormLabels';
-
-import AbandonedCart from '../../components/abandonedCart/AbandonedCart';
-import AdslStep from './wizard/steps/adslStep/AdslStep';
-import CheckoutStep from './wizard/steps/checkoutStep/CheckoutStep';
-import MobileStep from './wizard/steps/mobileStep/MobileStep';
-import NbnStep from './wizard/steps/nbnStep/NbnStep';
-import PlanConfigStep from './wizard/steps/planConfigStep/PlanConfigStep';
-import SummaryStep from './wizard/steps/summaryStep/SummaryStep';
-
 class OrderForm extends Component {
   // TODO: create this var dynamic from plan configuration
   userConfig = [
@@ -34,110 +24,15 @@ class OrderForm extends Component {
   //   console.log('OrderForm', this.props);
   // }
 
-  // Methods
-  getSteps = () => {
-    let steps = [];
-    for (let i = 0; i < this.userConfig.length; i++) {
-      steps.push(this.getStep(i, this.userConfig[i]));
-    }
-    return steps;
-  };
-
-  getStep = (index, stepId) => {
-    let step;
-    switch (stepId) {
-      case STEP_ROUTES.abanCart:
-        step = new StepModel(
-          index,
-          STEP_ROUTES.abanCart,
-          MENU_LABELS.abanCart,
-          <AbandonedCart onAction={() => this.onStepAction(stepId)} />
-        );
-        break;
-      case STEP_ROUTES.adsl:
-        step = new StepModel(
-          index,
-          STEP_ROUTES.adsl,
-          MENU_LABELS.adsl,
-          <AdslStep onAction={() => this.onStepAction(stepId)} />
-        );
-        break;
-      case STEP_ROUTES.mobile:
-        step = new StepModel(
-          index,
-          STEP_ROUTES.mobile,
-          'Mobile',
-          <MobileStep onAction={() => this.onStepAction(stepId)} />
-        );
-        break;
-      case STEP_ROUTES.nbn:
-        step = new StepModel(
-          index,
-          STEP_ROUTES.nbn,
-          'NBN',
-          <NbnStep onAction={() => this.onStepAction(stepId)} />
-        );
-        break;
-      case STEP_ROUTES.summary:
-        step = new StepModel(
-          index,
-          STEP_ROUTES.summary,
-          'Summary',
-          <SummaryStep onAction={() => this.onStepAction(stepId)} />
-        );
-        break;
-      case STEP_ROUTES.checkout:
-        step = new StepModel(
-          index,
-          STEP_ROUTES.checkout,
-          'Checkout',
-          <CheckoutStep onAction={() => this.onStepAction(stepId)} />
-        );
-        break;
-      default:
-        step = new StepModel(
-          index,
-          STEP_ROUTES.plan,
-          'Plan Configurator',
-          <PlanConfigStep onAction={() => this.onStepAction(stepId)} />
-        );
-    }
-    return step;
-  };
-
-  onStepAction = stepId => {
-    switch (stepId) {
-      case STEP_ROUTES.abanCart:
-        console.log(STEP_ROUTES.abanCart, 'actions');
-        break;
-      case STEP_ROUTES.adsl:
-        console.log(STEP_ROUTES.adsl, 'actions');
-        break;
-      case STEP_ROUTES.mobile:
-        console.log(STEP_ROUTES.mobile, 'actions');
-        break;
-      case STEP_ROUTES.nbn:
-        console.log(STEP_ROUTES.nbn, 'actions');
-        break;
-      case STEP_ROUTES.summary:
-        console.log(STEP_ROUTES.summary, 'actions');
-        break;
-      case STEP_ROUTES.checkout:
-        console.log(STEP_ROUTES.checkout, 'actions');
-        break;
-      default:
-        console.log('Step general actions');
-    }
-  };
-
   render() {
     return (
       <div className="row order-form">
+        {/*
         <Timeline steps={this.getSteps()} />
+        */}
         <div className="col-sm-8">
-          <WizardContainer steps={this.getSteps()} />
+          <WizardContainer userConfig={this.userConfig} labels={MENU_LABELS} routes={STEP_ROUTES} />
         </div>
-
         <div className="col-sm-4">
           <ShoppingCart />
         </div>
--- src/scenes/orderForm/wizard/WizardContainer.js
+++ src/scenes/orderForm/wizard/WizardContainer.js
@@ -3,19 +3,127 @@ import { Route, withRouter } from 'react-router-dom';
 import { Wizard, Steps, Step } from 'react-albus';
 import WizardNavigation from './wizardNavigation/WizardNavigation';
 
+import StepModel from './steps/StepModel';
+
+import AbandonedCart from '../../../components/abandonedCart/AbandonedCart';
+import AdslStep from './steps/adslStep/AdslStep';
+import CheckoutStep from './steps/checkoutStep/CheckoutStep';
+import MobileStep from './steps/mobileStep/MobileStep';
+import NbnStep from './steps/nbnStep/NbnStep';
+import PlanConfigStep from './steps/planConfigStep/PlanConfigStep';
+import SummaryStep from './steps/summaryStep/SummaryStep';
+
 const WizardContainer = props => {
   /* TODO: 
   * Dinamic flow depending on user selection in Plan configurator Check build in functionality: https://github.com/americanexpress/react-albus#wizard
   * Add routing guard on navigations buttons
   */
   // console.log('WizardContainer', props);
-  const steps = props.steps.map(step => {
-    return (
-      <Step key={step.id} id={step.id}>
-        {step.component}
-      </Step>
-    );
-  });
+  // Methods
+
+  const getSteps = () => {
+    let steps = [];
+    for (let i = 0; i < props.userConfig.length; i++) {
+      steps.push(getStep(i, props.userConfig[i]));
+    }
+    return steps;
+  };
+
+  const getStep = (index, stepId) => {
+    let step;
+    switch (stepId) {
+      case props.routes.abanCart:
+        step = new StepModel(
+          index,
+          props.routes.abanCart,
+          props.labels.abanCart,
+          <AbandonedCart onAction={() => onStepAction(stepId)} />
+        );
+        break;
+      case props.routes.adsl:
+        step = new StepModel(
+          index,
+          props.routes.adsl,
+          props.labels.adsl,
+          <AdslStep onAction={() => onStepAction(stepId)} />
+        );
+        break;
+      case props.routes.mobile:
+        step = new StepModel(
+          index,
+          props.routes.mobile,
+          'Mobile',
+          <MobileStep onAction={() => onStepAction(stepId)} />
+        );
+        break;
+      case props.routes.nbn:
+        step = new StepModel(
+          index,
+          props.routes.nbn,
+          'NBN',
+          <NbnStep onAction={() => onStepAction(stepId)} />
+        );
+        break;
+      case props.routes.summary:
+        step = new StepModel(
+          index,
+          props.routes.summary,
+          'Summary',
+          <SummaryStep onAction={() => onStepAction(stepId)} />
+        );
+        break;
+      case props.routes.checkout:
+        step = new StepModel(
+          index,
+          props.routes.checkout,
+          'Checkout',
+          <CheckoutStep onAction={() => onStepAction(stepId)} />
+        );
+        break;
+      default:
+        step = new StepModel(
+          index,
+          props.routes.plan,
+          'Plan Configurator',
+          <PlanConfigStep onAction={() => onStepAction(stepId)} />
+        );
+    }
+    return step;
+  };
+
+  const onStepAction = stepId => {
+    switch (stepId) {
+      case props.routes.abanCart:
+        console.log(props.routes.abanCart, 'actions');
+        break;
+      case props.routes.adsl:
+        console.log(props.routes.adsl, 'actions');
+        break;
+      case props.routes.mobile:
+        console.log(props.routes.mobile, 'actions');
+        break;
+      case props.routes.nbn:
+        console.log(props.routes.nbn, 'actions');
+        break;
+      case props.routes.summary:
+        console.log(props.routes.summary, 'actions');
+        break;
+      case props.routes.checkout:
+        console.log(props.routes.checkout, 'actions');
+        break;
+      default:
+        console.log('Step general actions');
+    }
+  };
+
+  let steps = getSteps();
+  // const steps = props.steps.map(step => {
+  //   return (
+  //     <Step key={step.id} id={step.id}>
+  //       {step.component}
+  //     </Step>
+  //   );
+  // });
 
   return (
     <div className="wizard-container">
@@ -23,7 +131,7 @@ const WizardContainer = props => {
         render={({ history }) => (
           <Wizard history={history}>
             <div className="steps">
-              <Steps>{steps}</Steps>
+              <Steps>{getSteps().map(step => step.component)}</Steps>
             </div>
             <WizardNavigation />
           </Wizard>
